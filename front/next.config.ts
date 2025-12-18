924bd63b332113b1f8e1cebef0957cbcdfbe12df
karenmmedina <kameme115@gmail.com>
2025-12-17 23:32:20 -0300

    Update Next.js configuration and dependencies; upgraded Next.js and ESLint packages to version 16.0.10, modified image remote patterns in the config, and adjusted TypeScript settings for improved JSX handling and project structure.

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,50 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static1.pocketnowimages.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'e7.pngegg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'purepng.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'wallpapers.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'maximstore.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.apple.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-ipoint.waugi.com.ar',
-      },
+      { protocol: "https", hostname: "static1.pocketnowimages.com" },
+      { protocol: "https", hostname: "e7.pngegg.com" },
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "purepng.com" },
+      { protocol: "https", hostname: "wallpapers.com" },
+      { protocol: "https", hostname: "maximstore.com" },
+      { protocol: "https", hostname: "www.apple.com" },
+      { protocol: "https", hostname: "cdn-ipoint.waugi.com.ar" },
     ],
   },
-  // images: {
-  //   loader: 'custom',
-  //   path: ""
-  // },
-}
+};
 
 export default nextConfig;
